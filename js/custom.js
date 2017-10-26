9fc8113d4a45f3fa29b1f4c3269df803657b03d7
Giles Bonner <gjbonner@gmail.com>
2017-10-26 11:55:44 -0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+/* Smooth scrolling */
+
+
+$(document).ready(function () {
+
+    $('.nav').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+            location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
+
+});
+
+
+$(document).ready(function () {
+
+    $('.start-btn').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+            location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
+
+});
+
+
