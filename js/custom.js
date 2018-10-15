03cd55bf9de2fc0ed9a54c5980607b3f1d3d8481
unknown <gjbonner@gmail.com>
2018-10-15 10:39:13 -0400

    'asdas'

--- js/custom.js
+++ js/custom.js
@@ -40,4 +40,26 @@ $(document).ready(function () {
 
 });
 
+if ($('.back-to-top').length) {
+    var scrollTrigger = 1100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                    console.log('its running')
+                $('.back-to-top').addClass('show');
+            } else {
+                $('.back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('.back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
 
